7bf6256872ac0a70330e7bdb3d4d90858335ac97
Chris Cardle <cdcardle@gmail.com>
2018-09-26 16:12:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = "Me"
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
-const shortGreeting = "Welcome, " + currentUser.slice(0, 1) + "!"
+const shortGreeting = "Welcome, #{currentUser.slice(0, 1)}!"
