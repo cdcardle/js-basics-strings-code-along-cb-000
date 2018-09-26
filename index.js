389c586eb4377c75ad726eb104c18b2fbef06c79
Chris Cardle <cdcardle@gmail.com>
2018-09-26 16:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = "Chris"
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.capitalize + "!"
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
