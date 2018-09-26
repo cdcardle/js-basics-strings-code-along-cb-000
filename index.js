e3ca000991d295b7916d465ac89d61d33dfe85c2
Chris Cardle <cdcardle@gmail.com>
2018-09-26 16:09:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let currentUser = "Me"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!"
-let shortGreeting = "Welcome, " + currentUser.first + "!"
+let shortGreeting = "Welcome, " + currentUser.slice(o,1) + "!"
